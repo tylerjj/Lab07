c875846963ba05759d84654290f0d2bc3406c640
Tyler J. Johnston <tylerj.johnston@outlook.com>
2021-11-15 11:38:30 -0600

    Milestone 01

---
+++ app/src/main/java/com/example/lab07/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.lab07;
+
+import static com.example.lab07.App.CHANNEL_1_ID;
+import static com.example.lab07.App.CHANNEL_2_ID;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTitle = (EditText) findViewById(R.id.TitleInput);
+        editTextMessage = (EditText) findViewById(R.id.NameInput);
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    public void sendOnChannel1(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat
+                .Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_twotone_chat_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat
+                .Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_baseline_mood_bad_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(2, notification);
+    }
+}
